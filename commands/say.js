a4ba2168de51d171ec55ff5802c6e119876a7e58
spookyfied <44488883+spookyfied@users.noreply.github.com>
2018-10-26 20:12:10 -0400

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,5 +1,13 @@
 const Discord = require("discord.js");
 const errors = require("../utils/errors.js");
+const embed = new Discord.RichEmbed()
+
+let botembed = new Discord.RichEmbed()
+  .setDescription("Bot Info")
+  .setColor("#f44e42")
+  .addField("Bot Name", bot.user.username)
+
+  return message.channel.send(botembed);
 
 module.exports.run = async (bot, message, args) => {
 
