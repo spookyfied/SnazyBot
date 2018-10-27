b6d7b9018829cc8de6f32ac723ff1baba2612266
spookyfied <44488883+spookyfied@users.noreply.github.com>
2018-10-26 20:30:01 -0400

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ fs.readdir("./commands/", (err, files) => {
 bot.on("ready", async () => {
 
   console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
-  bot.user.setActivity("NBA 2K19", {type: "PLAYING"});
+  bot.user.setActivity("NBA 2K19", {type: "WATCHING"});
 
 });
 
