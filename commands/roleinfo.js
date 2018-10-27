694afcbc26d4fd9d29f5c91f1ce771ff34051cde
spookyfied <44488883+spookyfied@users.noreply.github.com>
2018-10-26 20:49:47 -0400

    Update roleinfo.js

--- commands/roleinfo.js
+++ commands/roleinfo.js
@@ -4,7 +4,7 @@ exports.run = (client, message, args) => {
     let role = message.mentions.roles.first() || message.guild.roles.get(args[0]) || message.guild.roles.find(role => role.name === args[0]);
 
     // If we can't find any role, then just default to the author's highest role
-    if (-role) role = message.member.highestRole;
+    if (!role) role = message.member.highestRole;
 
 
     // Define our embed
