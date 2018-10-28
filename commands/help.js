9038b7ee013200070110cf0065da54002712a6e6
spookyfied <44488883+spookyfied@users.noreply.github.com>
2018-10-28 18:13:37 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,14 +1,27 @@
-    module.exports.help = {
-        name: "help"
+const settings = require('../settings.json');
+exports.run = (client, message, params) => {
+  if (!params[0]) {
+    const commandNames = Array.from(client.commands.keys());
+    const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
+    message.channel.send(`= Command List =\n\n[Use ${settings.prefix}help <commandname> for details]\n\n${client.commands.map(c => `${settings.prefix}${c.help.name}${' '.repeat(longest - c.help.name.length)} :: ${c.help.description}`).join('\n')}`, {code:'asciidoc'});
+  } else {
+    let command = params[0];
+    if (client.commands.has(command)) {
+      command = client.commands.get(command);
+      message.channel.send(`= ${command.help.name} = \n${command.help.description}\nusage::${command.help.usage}`, {code:'asciidoc'});
     }
+  }
+};
 
-const Discord = require("discord.js");
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['h', 'halp'],
+  permLevel: 0
+};
 
-exports.run = async (client, message, args, tools, con) => {
-    try {
-        await message.author.send(`Commands: \n\n${client.commands.map(cmd => `\`${cmd.help.name}\``).join(", ")}`);
-        message.channel.send("Check your dm's for a list of bot commands.");
-    } catch (e) {
-        throw e;
-    }
-}
+exports.help = {
+  name: 'help',
+  description: 'Displays all the available commands for your permission level.',
+  usage: 'help [command]'
+};
