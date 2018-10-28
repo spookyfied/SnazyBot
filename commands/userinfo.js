7659d3abbabe710ee1e02e5558cae9261854f5d4
spookyfied <44488883+spookyfied@users.noreply.github.com>
2018-10-28 17:48:01 -0400

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,33 +1,53 @@
-//Define discord-js
-const Discord = require('discord.js');
+//Discord.js Version 12.0.0 MASTER
+//Npm Discord.js Pakage
+const Discord = require('discord.js'); // To Install Discord.js | Run This Command in Console/Terminal `npm install --save discordjs/discord.js`
+const client = new Discord.Client();
 
-//Define moment
-const moment = require("moment");
+let PREFIX = '-' //Prefix Can Be Any 
 
-exports.run = async (client, message, args) => {
-	let user;
-	// If the user mentions someone, display their stats. If they just run userinfo without mentions, it will show their own stats.
-    if (message.mentions.users.first()) {
-      user = message.mentions.users.first();
-    } else {
-        user = message.author;
+client.on('message', message => { //Message Event | Listener
+
+    if (message.content.startsWith(PREFIX + 'Userinfo')) {
+
+        const UserInfo = new Discord.MessageEmbed()
+
+            //All Fields are Optional Pick Any some
+
+            .setAuthor(message.author.username, message.author.avatarURL()) //Heading With Username & Their Avatar 
+            .setTitle('UserInfo')
+            .setURL('www.google.com') //Any Vaild Link
+            .setColor('RANDOM') //You Can Use HexColour Ex:- #000000
+            .setImage(message.author.avatarURL()) //Add Any Image URl || Image
+            .setThumbnail(message.author.avatarURL()) //Add Any Image URl || ThumbNail
+
+            //All Feilds Are Just Examples pick Some & add as you like
+
+            .addField('Avatar', message.author.avatar, true) //The ID of the user's avatar //Inline True or false
+            .addField('AvatarURL', message.author.avatarURL({
+                format: 'png'
+            }), true) //{options} options are Size?: 128 | 256 | 512 | 1024 | 2048, Format?: "webp" | "png" | "jpg" | "gif" //.defaultAvatarURL() A link to the user's default avatar //.displayAvatarURL() A link to the user's avatar if they have one. Otherwise a link to their default avatar will be returned
+            .addField('AvatarURL', message.author.avatarURL({
+                size: '2048'
+            }), true)
+            .addField('Bot', message.author.bot, true) //Returns True If Message Author = Bot || False If Message Author not Bot.
+            .addField('Created At', message.author.createdAt, false) //The time the user was created || .createdTimestamp - The timestamp the user was created at
+            .addField('Discrim', message.author.discriminator, true) //A discriminator/tag based on username for the user Ex:- 0001
+            .addField('DMChannel', message.author.dmChannel) //The DM between the client's user and this user || If Nothing Returns "Null"
+            .addField('ID', message.author.id) //The ID of the User/author
+            .addField('Last Message', message.author.lastMessage) //The Message object of the last message sent by the user, if one was sent
+            .addField('Last Message ID', message.author.lastMessageID) //The ID of the last message sent by the user, if one was sent
+            .addField('Presence', message.author.presence) //The presence of this user
+            .addField('Presence Status', message.author.presence.status) //The presence status of this user
+            .addField('Presence Game', message.author.presence.activity.name) //The presence Game of this user
+            .addField('Tag', message.author.tag) //The Discord "tag" for this user || Ex:- Sai Chinna#6718
+            .addField('Username', message.author.username) //The username of the user || Ex:- Sai Chinna
+            .addField('Nick Name', message.guild.member(target).displayName) //Nick Name In That (message sent) server || Define target as message Author Ex:- let target = message.author; || Add This Line in Top
+
+            .setFooter('Requested By', message.author.tag) //Change To Anything As You Wish
+            .setTimestamp() //The timestamp of this embed
+
+        message.channel.send(UserInfo);
     }
-	// Define the member of a guild.
-    const member = message.guild.member(user);
-	
-	//Discord rich embed
-    const embed = new Discord.RichEmbed()
-		.setColor('RANDOM')
-		.setThumbnail(user.avatarURL)
-		.setTitle(`${user.username}#${user.discriminator}`)
-		.addField("ID:", `${user.id}`, true)
-		.addField("Nickname:", `${member.nickname !== null ? `${member.nickname}` : 'None'}`, true)
-		.addField("Created At:", `${moment.utc(user.createdAt).format('dddd, MMMM Do YYYY, HH:mm:ss')}`, true)
-		.addField("Joined Server:", `${moment.utc(member.joinedAt).format('dddd, MMMM Do YYYY, HH:mm:ss')}`, true)
-		.addField("Bot:", `${user.bot}`, true)
-		.addField("Status:", `${user.presence.status}`, true)
-		.addField("Game:", `${user.presence.game ? user.presence.game.name : 'None'}`, true)
-		.addField("Roles:", member.roles.map(roles => `${roles.name}`).join(', '), true)
-		.setFooter(`Replying to ${message.author.username}#${message.author.discriminator}`)
-     message.channel.send({embed});
-    }
+});
+
+bot.login(process.env.BOT_TOKEN)
