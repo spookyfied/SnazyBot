4e544ffb1d7d5d8c009a436bfc1d9ea3d22f4970
spookyfied <44488883+spookyfied@users.noreply.github.com>
2018-11-04 01:55:16 -0400

    Update embed.js

--- commands/embed.js
+++ commands/embed.js
@@ -8,7 +8,6 @@ module.exports.run = async (client, message, args) => {
         .setTimestamp();
 
     if (!args[0]) return message.channel.send(specifyembed);
-      let embedsay = args.join(" ");
 
     let embedsay = new Discord.RichEmbed()
         .setColor('RANDOM')
